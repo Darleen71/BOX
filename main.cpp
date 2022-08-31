ffb31e7a9b7115bd0cbb4edde408a293188984f4
Darleen71 <l13262698128@163.com>
2022-08-31 15:28:36 +0800

    The second time commit.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
     cout << "hello git!!!!!!" << endl;
     cout << "new" << endl;
+    cout << "hello git!!!!!!" << endl;
+    cout << "hello git!!!!!!" << endl;
     system("pause");
     return 0;
 }
