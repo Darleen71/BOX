72be4af171756f5a4ec3de9a0ebbe677e1ef57b3
Darleen71 <103994902+Darleen71@users.noreply.github.com>
2022-09-01 15:16:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(){
     cout << "hello git!!!!!!" << endl;
     cout << "new" << endl;
     cout << "new master" << endl;
+    cout << "edit for new pull" << endl;
     system("pause");
     return 0;
 }
